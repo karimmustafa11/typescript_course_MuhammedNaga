674a23b0555c2afab8ad222fb3d31719de4302ba
karimmustafa <karimmustafa495@gmail.com>
2025-04-02 06:55:13 +0200

    Simple typescript syntax

---
+++ src/Object_Array_Tuple_Void.ts
@@ -0,0 +1,18 @@
+//object syntax
+const obj: { name: string, email: string, age: number } = {
+    name: "KARIM",
+    email: "karim@gmail.com",
+    age: 4
+}
+
+//Array syntax
+let myarray: number[] = [1, 2, 3, 4, 5]
+
+
+//tuple syntax
+let list: [number, string, { password: number }, istrue: boolean]
+
+//void 
+function myfun(message: string): void {
+    console.log(message)
+}
