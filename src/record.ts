f21895e24c2d46bee1805baf3b6a372e138a4f49
karimmustafa <karimmustafa495@gmail.com>
2025-04-03 02:13:10 +0200

    Record Syntax

---
+++ src/record.ts
@@ -0,0 +1,9 @@
+// ينفع تضيف بروبيرتي ف الجافا سكريبت من برا عادي
+// ف التايب هتطلع ايرور يبقي الحل استخدم ريكورد
+
+//Record Syntax
+
+const person: Record<string, string | number> = {
+    username: "karim"
+}
+person["age"] = 30
